d42fd4b7b791e42efff65d779640010ede46901a
Scott Randolph <c.scott.randolph@gmail.com>
2021-09-25 15:05:38 -0600

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -24,9 +24,9 @@ function compute()
 	// if a positive number was entered display the result.
 	else {	
 		document.getElementById("result").innerHTML =
-		"If you deposit " + "<strong>" + principal + "</strong>" + ",\<br\>at an interest rate of "
-		+ "<strong>" + rate + "</strong>" +"%\<br\>You will receive an amount of " + "<strong>" + amount + "</strong>" +
-		",\<br\>in the year " + "<strong>" + year + "</strong>" + "\<br\>";
+		"If you deposit " + "<mark>" + principal + "</mark>" + ",\<br\>at an interest rate of "
+		+ "<mark>" + rate + "</mark>" +"%\<br\>You will receive an amount of " + "<mark>" + amount + "</mark>" +
+		",\<br\>in the year " + "<mark>" + year + "</mark>" + "\<br\>";
 	}
 }
 
